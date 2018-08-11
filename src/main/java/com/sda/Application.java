0aec675b4a07ff6b883e3729a4b08931c67bbc0a
tommydrama <tomek1darowski@gmail.com>
2018-08-11 11:54:52 +0200

    builder #1

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -1,7 +1,41 @@
 package com.sda;
 
+import com.sda.model.Document;
+
+import java.time.Instant;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
+
+        //builder
+        //all args constructor
+        Document document = new Document("Plan dnia",
+                "Opis dnia szkoleniowego",
+                "Tomasz Darowski",
+                Instant.now(),
+                Arrays.asList("długopis", "zeszyt", "laptop"));
+
+        //setters
+        Document documentBySetters = new Document();
+        documentBySetters.setTitle("Plan Tygodnia");
+        documentBySetters.setDescription("No nieźle");
+        documentBySetters.setAuthor("Janusz Polak");
+        documentBySetters.setCreationDate(Instant.now());
+        documentBySetters.setItems(Arrays.asList("elo", "pozdro", "-_-"));
+
+        //builder
+        Document documentByBuilder = Document.builder().
+                title("Plan zajęć").
+                description("coś tam").
+                creationDate(Instant.now()).
+                items(Arrays.asList("beka", "yolo")).
+                item("beka123").
+                item("beka321").
+                author("Tomasz Darowski").
+                build();
+
+
         System.out.println("Hello World");
     }
 }
---
+++ src/main/java/com/sda/model/Document.java
@@ -0,0 +1,104 @@
+package com.sda.model;
+
+import java.time.Instant;
+import java.util.List;
+
+public class Document {
+
+    private String title;
+    private String description;
+    private String author;
+    private Instant creationDate;
+    private List<String> items;
+
+    public static DocumentBuilder builder(){
+        return new DocumentBuilder();
+    }
+
+    public Document() {
+    }
+
+    public Document(String title, String description, String author, Instant creationDate, List <String> items) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.creationDate = creationDate;
+        this.items = items;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List <String> getItems() {
+        return items;
+    }
+
+    public void setItems(List <String> items) {
+        this.items = items;
+    }
+
+    public static class DocumentBuilder{
+        private String title;
+        private String description;
+        private String author;
+        private Instant creationDate;
+        private List<String> items;
+
+        public DocumentBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+        public DocumentBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+        public DocumentBuilder author(String author) {
+            this.author = author;
+            return this;
+        }
+        public DocumentBuilder creationDate(Instant creationDate) {
+            this.creationDate = creationDate;
+            return this;
+        }
+        public DocumentBuilder items(List<String> items) {
+            this.items.addAll(items);
+            return this;
+        }
+        public  DocumentBuilder item (String item){
+            this.items.add(item);
+            return this;
+        }
+        public Document build(){
+            return new Document(title, description, author, creationDate, items);
+        }
+    }
+}
